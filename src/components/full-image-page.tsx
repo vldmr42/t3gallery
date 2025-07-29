44bf1a226e3038e6db91760cfdffd6a51ea75a59
vldmr42 <kmb14056@yandex.ru>
2025-07-29 21:11:06 +0400

    modal styling

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,7 +1,15 @@
-import { Modal } from "~/app/@modal/(.)img/[id]/modal";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img src={image.url} className="w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} className="object-contain" />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col items-center justify-center border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
